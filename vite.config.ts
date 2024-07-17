2dbc15a734eec0aed886d355e81561935d82693f
mor-michael <139643327+mor-michael@users.noreply.github.com>
2024-07-17 20:46:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "minesweeper-react",
   plugins: [react()],
+  base: "https://mor-michael.github.io/minesweeper-react/",
 })
